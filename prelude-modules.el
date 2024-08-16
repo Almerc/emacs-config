070b6d5315ea5a04d3ad7779b84b4efde54480b8
Alex Watkinson <alexwatkinson@mail.com>
2024-08-16 15:34:08 +0100

    Create prelude-modules.el

---
+++ prelude-modules.el
@@ -0,0 +1,44 @@
+(require 'prelude-vertico) ;; A powerful, yet simple, alternative to ivy
+(require 'prelude-helm) ;; Interface for narrowing and search
+(require 'prelude-helm-everywhere) ;; Enable Helm everywhere
+(require 'prelude-company)
+(require 'prelude-org)
+
+;;; Programming languages support
+;;
+;; Modules for a few very common programming languages
+;; are enabled by default.
+
+;; (require 'prelude-c)
+(require 'prelude-clojure)
+;; (require 'prelude-coffee)
+;; (require 'prelude-common-lisp)
+(require 'prelude-css)
+;; (require 'prelude-dart)
+(require 'prelude-emacs-lisp)
+;; (require 'prelude-erlang)
+;; (require 'prelude-elixir)
+;; (require 'prelude-go)
+;; (require 'prelude-haskell)
+(require 'prelude-js)
+;; (require 'prelude-latex)
+(require 'prelude-lisp) ;; Common setup for Lisp-like languages
+(require 'prelude-lsp) ;; Base setup for the Language Server Protocol
+;; (require 'prelude-lua)
+;; (require 'prelude-ocaml)
+(require 'prelude-perl)
+(require 'prelude-python)
+;; (require 'prelude-racket)
+;; (require 'prelude-ruby)
+(require 'prelude-rust)
+;; (require 'prelude-scala)
+;; (require 'prelude-scheme)
+(require 'prelude-shell)
+(require 'prelude-scss)
+(require 'prelude-ts)
+(require 'prelude-web) ;; Emacs mode for web templates
+(require 'prelude-xml)
+(require 'prelude-yaml)
+
+;;; Misc
+(require 'prelude-erc) ;; A popular Emacs IRC client (useful if you're still into Freenode)
